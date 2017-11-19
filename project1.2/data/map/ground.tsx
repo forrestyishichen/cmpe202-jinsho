42e9db021042354b9d2bdf53f8f30ced82e598e8
forrestyisshichen <forrestyschen@gmail.com>
2017-11-18 21:22:48 -0800

    add v1.4 and remove old folder
    Updated new game resource.

--- project1.2/data/map/ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="spritesheet_ground" tilewidth="128" tileheight="128" tilecount="128" columns="8">
- <image source="../img/map/spritesheet_ground.png" width="1024" height="2048"/>
-</tileset>
